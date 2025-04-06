538f50b4eb745341b3d30c61c83b0965365de1a2
Balasis <giovani1994a@gmail.com>
2025-04-06 06:34:12 +0300

    wrong import

--- src/server.test.js
+++ src/server.test.js
@@ -1,10 +1,23 @@
-// src/index.test.js
-const request = require('supertest'); 
-const app = require('./server'); 
+// index.test.js
+import request from 'supertest';
+import runServer from './server'; 
 
 describe('Server running test', () => {
+  let app;
+
+  beforeAll(() => {
+    // Setup the server before running the tests
+    const handlers = {
+      info: () => 'Battlesnake API',
+      start: () => {},
+      move: () => {},
+      end: () => {},
+    };
+    app = runServer(handlers);
+  });
+
   it('should return status 200 on GET /', async () => {
-    const response = await request(app).get('/'); 
-    expect(response.status).toBe(200); 
+    const response = await request(app).get('/');
+    expect(response.status).toBe(200);
   });
 });
